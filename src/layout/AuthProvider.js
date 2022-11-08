34d2e79f21c69a7aa93cac6d748c091806df0298
Abu Talha <abutalha141999@gmail.com>
2022-11-08 14:36:50 +0600

    authContext integrated firebase setup and integrated and function setup

--- src/layout/AuthProvider.js
+++ src/layout/AuthProvider.js
@@ -1,7 +1,38 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/auth.cofig";
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const auth = getAuth(app);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+  const existUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const updateUserProfile = (name) => {
+    return updateProfile(auth.currentUser, name);
+  };
+  const authInfo = {
+    user,
+    createUser,
+    existUser,
+    updateUserProfile
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
